d0e52b3d0e5da87b453ba65ef94be5a95d88dedf
Itang Sanjana <arwata@gmail.com>
2016-03-30 11:29:24 +0700

    Creating the Model

---
+++ app/src/main/java/io/github/itangsanjana/stormy/CurrentWeather.java
@@ -0,0 +1,52 @@
+package io.github.itangsanjana.stormy;
+
+/**
+ * Created by itangsanjana on 30/03/16.
+ */
+public class CurrentWeather {
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mPrecipChance;
+    private String mSummary;
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getPrecipChance() {
+        return mPrecipChance;
+    }
+
+    public void setPrecipChance(double precipChance) {
+        mPrecipChance = precipChance;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+}
