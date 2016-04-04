1dbf86f21a4bc78a6d43ad2df36d368e1e42aee4
Itang Sanjana <arwata@gmail.com>
2016-04-05 03:01:00 +0700

    Using a ListView in a Regular Activity

--- app/src/main/java/io/github/itangsanjana/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/io/github/itangsanjana/stormy/ui/DailyForecastActivity.java
@@ -6,42 +6,50 @@ import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Arrays;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import io.github.itangsanjana.stormy.R;
 import io.github.itangsanjana.stormy.adapters.DayAdapter;
 import io.github.itangsanjana.stormy.weather.Day;
 
-public class DailyForecastActivity extends ListActivity {
+public class DailyForecastActivity extends AppCompatActivity {
 
     private Day[] mDays;
+
+    @Bind(android.R.id.list) ListView mListView;
+    @Bind(android.R.id.empty) TextView mTextViewEmpty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
         mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
 
         DayAdapter adapter = new DayAdapter(this, mDays);
-        setListAdapter(adapter);
-    }
+        mListView.setAdapter(adapter);
+        mListView.setEmptyView(mTextViewEmpty);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String dayOfTheWeek = mDays[position].getDayOfTheWeek();
+                String summary = mDays[position].getSummary();
+                String temperatureMax = mDays[position].getTemperatureMax() + "";
+                String message = String.format("On %s the high will be %s and it will be %s", dayOfTheWeek, temperatureMax, summary);
 
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-
-        String dayOfTheWeek = mDays[position].getDayOfTheWeek();
-        String summary = mDays[position].getSummary();
-        String temperatureMax = mDays[position].getTemperatureMax() + "";
-        String message = String.format("On %s the high will be %s and it will be %s", dayOfTheWeek, temperatureMax, summary);
-
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+                Toast.makeText(DailyForecastActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
