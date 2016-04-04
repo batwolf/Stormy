ac4369beefaa55f3c3e2479a805cb4ac0061938a
Itang Sanjana <arwata@gmail.com>
2016-04-04 21:30:30 +0700

    Making Hour Parcelable

--- app/src/main/java/io/github/itangsanjana/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/io/github/itangsanjana/stormy/ui/HourlyForecastActivity.java
@@ -1,15 +1,26 @@
 package io.github.itangsanjana.stormy.ui;
 
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Arrays;
+
 import io.github.itangsanjana.stormy.R;
+import io.github.itangsanjana.stormy.weather.Hour;
 
 public class HourlyForecastActivity extends AppCompatActivity {
+
+    private Hour[] mHours;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
+        mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
     }
 }
--- app/src/main/java/io/github/itangsanjana/stormy/weather/Hour.java
+++ app/src/main/java/io/github/itangsanjana/stormy/weather/Hour.java
@@ -1,15 +1,20 @@
 package io.github.itangsanjana.stormy.weather;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by itangsanjana on 01/04/16.
  */
-public class Hour {
+public class Hour implements Parcelable {
 
     private long mTime;
     private String mSummary;
     private double mTemperature;
     private String mIcon;
     private String mTimezone;
+
+    public Hour() { }
 
     public long getTime() {
         return mTime;
@@ -50,4 +55,38 @@ public class Hour {
     public void setTimezone(String timezone) {
         mTimezone = timezone;
     }
+
+    @Override
+    public int describeContents() {
+        return 0; // Ignore.
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(mTime);
+        dest.writeDouble(mTemperature);
+        dest.writeString(mSummary);
+        dest.writeString(mIcon);
+        dest.writeString(mTimezone);
+    }
+
+    private Hour(Parcel in) {
+        mTime = in.readLong();
+        mTemperature = in.readDouble();
+        mSummary = in.readString();
+        mIcon = in.readString();
+        mTimezone = in.readString();
+    }
+
+    public static final Creator<Hour> CREATOR = new Creator<Hour>() {
+        @Override
+        public Hour createFromParcel(Parcel source) {
+            return new Hour(source);
+        }
+
+        @Override
+        public Hour[] newArray(int size) {
+            return new Hour[size];
+        }
+    };
 }
