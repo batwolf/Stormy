c257c4093084cff4a6ecbb53a7d401523beefee1
Itang Sanjana <arwata@gmail.com>
2016-04-04 17:34:47 +0700

    Using a Creator and Reading a Parcel

--- app/src/main/java/io/github/itangsanjana/stormy/weather/Day.java
+++ app/src/main/java/io/github/itangsanjana/stormy/weather/Day.java
@@ -83,4 +83,24 @@ public class Day implements Parcelable {
         dest.writeString(mIcon);
         dest.writeString(mTimezone);
     }
+
+    private Day(Parcel in) {
+        mTime = in.readLong();
+        mSummary = in.readString();
+        mTemperatureMax = in.readDouble();
+        mIcon = in.readString();
+        mTimezone = in.readString();
+    }
+
+    public static final Creator<Day> CREATOR = new Creator<Day>() {
+        @Override
+        public Day createFromParcel(Parcel source) {
+            return new Day(source);
+        }
+
+        @Override
+        public Day[] newArray(int size) {
+            return new Day[size];
+        }
+    }
 }
